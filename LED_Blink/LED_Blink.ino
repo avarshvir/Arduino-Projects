5f603fe9f2e49944cc2f3410914a938a18651238
Arshvir <avarshvir@gmail.com>
2024-07-31 20:46:52 +0530

    commit

--- LED_Blink/LED_Blink.ino
+++ LED_Blink/LED_Blink.ino
@@ -1,13 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13,HIGH);
+  pinMode(12,HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
   delay(500);   //delay for 0.5 seconds
-  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
   delay(500);   
 
 }
